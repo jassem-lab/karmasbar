b536e86b493d88f0547c4e216502758609199528
jassem-lab <jassemgaaloul123@gmail.com>
2021-05-23 23:11:02 +0100

    💄 Add Layout & SEO to pages

---
+++ src/pages/Deals.js
@@ -0,0 +1,13 @@
+import React from "react"
+import Layout from "../components/layout/layout"
+import SEO from "../components/seo"
+
+const Deals = () => {
+  return (
+    <Layout>
+      <SEO />
+    </Layout>
+  )
+}
+
+export default Deals
---
+++ src/pages/Menu.js
@@ -0,0 +1,13 @@
+import React from "react"
+import Layout from "../components/layout/layout"
+import SEO from "../components/seo"
+
+const Menu = () => {
+  return (
+    <Layout>
+      <SEO title="Menu" />
+    </Layout>
+  )
+}
+
+export default Menu
