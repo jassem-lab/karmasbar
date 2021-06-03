bb6c3f594c8591d64f73ce5e50b7ef8038412e7a
jassem-lab <jassemgaaloul123@gmail.com>
2021-06-03 09:51:47 +0100

    🚧 fixing useWindowSize hook ( deployment )

--- gatsby-node.js
+++ gatsby-node.js
@@ -143,17 +143,3 @@ async function getPosts({ graphql, reporter }) {
 
   return graphqlResult.data.allWpPost.edges
 }
-exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
-  if (stage === "build-html") {
-    actions.setWebpackConfig({
-      module: {
-        rules: [
-          {
-            test: /offending-module/,
-            use: loaders.null(),
-          },
-        ],
-      },
-    })
-  }
-}
