91485eaf6a8bec1cb94dc8e1abde43f4c233f80d
jassem-lab <jassemgaaloul123@gmail.com>
2021-06-03 10:01:36 +0100

    🚀 Deploy Stuff

--- src/components/layout/layout.js
+++ src/components/layout/layout.js
@@ -7,31 +7,31 @@ import Footer from "../footer/footer"
 import { motion, AnimatePresence } from "framer-motion"
 import useWindowSize from "../../hooks/useWindowSize"
 
-const { width } = useWindowSize()
-
-const duration = 0.2
-
-const variants = {
-  initial: {
-    opacity: 0,
-    x: -width,
-  },
-  enter: {
-    opacity: 1,
-    x: 0,
-    transition: {
-      duration: duration,
-      delay: duration,
-      when: "beforeChildren",
-    },
-  },
-  exit: {
-    opacity: 0,
-    transition: { duration: duration },
-  },
-}
-
 const Layout = ({ children }) => {
+  const { width } = useWindowSize()
+
+  const duration = 0.2
+
+  const variants = {
+    initial: {
+      opacity: 0,
+      x: -width,
+    },
+    enter: {
+      opacity: 1,
+      x: 0,
+      transition: {
+        duration: duration,
+        delay: duration,
+        when: "beforeChildren",
+      },
+    },
+    exit: {
+      opacity: 0,
+      transition: { duration: duration },
+    },
+  }
+
   const WpData = useStaticQuery(graphql`
     query SiteTitleQuery {
       site {
