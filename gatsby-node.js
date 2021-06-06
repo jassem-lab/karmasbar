1e6a9fba63d89dfa39681939c650b1da8ba483ac
jassem-lab <jassemgaaloul123@gmail.com>
2021-06-06 23:22:56 +0100

    🚩 fix mapbox

--- gatsby-node.js
+++ gatsby-node.js
@@ -10,7 +10,23 @@ const chunk = require(`lodash/chunk`)
  * It's purpose is to allow you to create pages for your site! 💡
  *
  * See https://www.gatsbyjs.com/docs/node-apis/#createPages for more info.
+ *
  */
+
+exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
+  if (stage === "build-html") {
+    actions.setWebpackConfig({
+      module: {
+        rules: [
+          {
+            test: /@mapbox|mapbox-gl/,
+            use: loaders.null(),
+          },
+        ],
+      },
+    })
+  }
+}
 exports.createPages = async gatsbyUtilities => {
   // Query our posts from the GraphQL server
   const posts = await getPosts(gatsbyUtilities)
