6a5fab3ca405f8a1ec4792fd237ef0634deca5a9
jassem-lab <jassemgaaloul123@gmail.com>
2021-06-10 01:26:23 +0100

    🔥 gatsby-plugin-purgecss

--- src/components/recaptcha/recaptcha.js
+++ src/components/recaptcha/recaptcha.js
@@ -1,15 +1,15 @@
 import React from "react"
-import ReCAPTCHA from "react-google-recaptcha"
+// import ReCAPTCHA from "react-google-recaptcha"
+import loadable from "@loadable/component"
 
-const onChange = value => {
-}
+const ReCAPTCHA = loadable(() => import("react-google-recaptcha"))
+
 
 const Recaptcha = () => {
   return (
     <div>
       <ReCAPTCHA
         sitekey="6Lexe-caAAAAAHK20M9q6kdvEvxMB05-6RIdUka5"
-        onChange={onChange}
       />
     </div>
   )
